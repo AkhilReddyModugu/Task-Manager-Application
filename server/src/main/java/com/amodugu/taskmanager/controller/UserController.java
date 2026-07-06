6d1f1ea6bd72894ceaf3c91e9431ba7e319f99c9
AkhilReddyModugu <akhilreddymodugu13@gmail.com>
2026-07-07 00:47:46 +0530

    Create User flow

---
+++ server/src/main/java/com/amodugu/taskmanager/controller/UserController.java
@@ -0,0 +1,37 @@
+package com.amodugu.taskmanager.controller;
+
+import com.amodugu.taskmanager.entity.User;
+import com.amodugu.taskmanager.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
+        return userService.getUserById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User saved = userService.createUser(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(saved);
+    }
+}
---
+++ server/src/main/java/com/amodugu/taskmanager/service/UserService.java
@@ -0,0 +1,29 @@
+package com.amodugu.taskmanager.service;
+
+import com.amodugu.taskmanager.entity.User;
+import com.amodugu.taskmanager.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+}
