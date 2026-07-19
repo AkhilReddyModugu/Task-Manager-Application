1116335f8f5dd5db1248cc4b9404a6702c51d21a
AkhilReddyModugu <akhilreddymodugu13@gmail.com>
2026-07-19 19:13:34 +0530

    Add Actuator

--- server/src/main/java/com/amodugu/taskmanager/config/SecurityConfig.java
+++ server/src/main/java/com/amodugu/taskmanager/config/SecurityConfig.java
@@ -37,6 +37,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v1/auth/**").permitAll()
+                        .requestMatchers("/actuator/health").permitAll()
                         .anyRequest().authenticated()
                 );
         http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
